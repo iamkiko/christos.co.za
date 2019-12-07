979ae1c140d349913397859847598e71865558d7
Christos <constantinides.ch@gmail.com>
2019-12-07 11:15:02 +0200

    renamed head import logo

--- src/components/head.js
+++ src/components/head.js
@@ -2,7 +2,7 @@ import React from "react"
 import { Helmet } from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
 
-import headlogo from "../assets/C.png"
+import headlogo from "../assets/c.png"
 const Head = ({ title }) => {
   const data = useStaticQuery(graphql`
     query {
